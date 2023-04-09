1bb9f7e4046c4757bed97c7ff9f3500c71db1747
Shaun Hathaway <Shaun.Hathaway@outlook.com>
2023-04-09 10:43:22 -0700

    reordered

---
+++ KampLibrary/Db/SqliteDb.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+
+public class BloggingContext : DbContext
+{
+    public DbSet<RecAreaAddressesEntries> RecAreaAddressesDbEntries { get; set; }
+    public DbSet<FacilitiesEntries> FacilitiesDbEntries { get; set; }
+    public string DbPath { get; }
+    public BloggingContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(path, "RecreationDotOrg.db");
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+}
