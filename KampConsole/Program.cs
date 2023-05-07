ac320d7d7011f2e0a1b32f8e4a2fb1cbf603a843
Shaun Hathaway <Shaun.Hathaway@outlook.com>
2023-05-07 10:11:09 -0700

    added overloads

--- KampConsole/Program.cs
+++ KampConsole/Program.cs
@@ -1,12 +1,4 @@
-﻿//using (var httpClient = new HttpClient())
-//{
-//    using (var response = await httpClient.GetAsync("https://localhost:44324/api/Reservation"))
-//    {
-//        string apiResponse = await response.Content.ReadAsStringAsync();
-//    }
-//}
-
-using System.Text.Json;
+﻿using System.Text.Json;
 
 class Program
 {
@@ -14,14 +6,9 @@ class Program
     {
         //KampLibrary.function.sqlite.Clear.All();
         //KampLibrary.function.sqlite.PopulateDbFromFile.Populate(@"C:\Users\Shaun\Desktop\RIDBFullExport_V1_JSON\");
-        int counter = 0;
-        while (counter < 3)
-        {
-            DateTime Pdt = DateTime.Now.AddMonths(counter);//.AddMonths(1));
-            DateTime ChkDt = Convert.ToDateTime($"{Pdt.Month}/1/{Pdt.Year} 0:00:00 AM");
-            KampLibrary.function.RecDotOrg.AvailabilityApi.GetAvailabilitiesByPark("232445", ChkDt);
-            counter++;
-        }
+
+        KampLibrary.function.RecDotOrg.AvailabilityApi.GetAvailabilitiesByPark("232445");//, 2, new List<string>() { "GROUP", "STANDARD NONELECTRIC" });
+
         Console.WriteLine("Done");
         Console.ReadLine();
     }
--- KampLibrary/function/RecDotOrg/AvailabilityApi.cs
+++ KampLibrary/function/RecDotOrg/AvailabilityApi.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
@@ -10,64 +11,105 @@ namespace KampLibrary.function.RecDotOrg
 {
     static public class AvailabilityApi
     {
-        public async static void GetAvailabilitiesByPark(String ParkID, DateTime CheckDt)
+        public static void GetAvailabilitiesByPark(String? ParkID)
         {
-            List<CampsitesRecdata> Sites = KampLibrary.function.sqlite.Read.GetCampsitesByFacility(ParkID);
-            String Url = $"https://www.recreation.gov/api/camps/availability/campground/{ParkID}/month?start_date={CheckDt.Year.ToString()}-{CheckDt.ToString("MM")}-01T00%3A00%3A00.000Z";
-
-            using (var httpClient = new HttpClient())
+            GetAvailabilitiesByPark(ParkID, 0, null, null);
+        }
+        public static void GetAvailabilitiesByPark(String? ParkID, Int32? MonthsToCheck)
+        {
+            GetAvailabilitiesByPark(ParkID, MonthsToCheck, null, null);
+        }
+        public static void GetAvailabilitiesByPark(String? ParkID, Int32? MonthsToCheck, List<String>? FilterOut)
+        {
+            GetAvailabilitiesByPark(ParkID, MonthsToCheck, FilterOut, null);
+        }
+        public async static void GetAvailabilitiesByPark(String? ParkID, Int32? MonthsToCheck, List<String>? FilterOut, List<String>? FilterIn)
+        {
+            if (ParkID != null)
             {
-                using (var response = httpClient.GetAsync(Url))
+                List<CampsitesRecdata> Sites = KampLibrary.function.sqlite.Read.GetCampsitesByFacility(ParkID);
+                int totalcounter = 0;
+                while (totalcounter <= MonthsToCheck)
                 {
-                    var apiResponse = await response.Result.Content.ReadAsStreamAsync();
-                    AvailabilityEntries? source = new AvailabilityEntries();
-                    using (StreamReader r = new StreamReader(apiResponse))
-                    {
-                        string json = KampLibrary.function.generic.Data.NormalizeApiJsonData(r.ReadToEnd(), ParkID, CheckDt);
-                        source = JsonSerializer.Deserialize<AvailabilityEntries>(json);
-                        if (source?.campsites != null)
-                        {
-                            foreach (var entry in source.campsites)
-                            {
-                                entry.GenerateDates(CheckDt);
-                            }
+                    DateTime Pdt = DateTime.Now.AddMonths(totalcounter);
+                    DateTime CheckDt = Convert.ToDateTime($"{Pdt.Month}/1/{Pdt.Year} 0:00:00 AM");
 
-                            Int32 DaysInCurrentMonth = DateTime.DaysInMonth(CheckDt.Year, CheckDt.Month);
-                            int counter = 0;
-                            while (counter < DaysInCurrentMonth)
+                    String Url = $"https://www.recreation.gov/api/camps/availability/campground/{ParkID}/month?start_date={CheckDt.Year.ToString()}-{CheckDt.ToString("MM")}-01T00%3A00%3A00.000Z";
+
+                    using (var httpClient = new HttpClient())
+                    {
+                        using (var response = httpClient.GetAsync(Url))
+                        {
+                            var apiResponse = await response.Result.Content.ReadAsStreamAsync();
+                            AvailabilityEntries? source = new AvailabilityEntries();
+                            using (StreamReader r = new StreamReader(apiResponse))
                             {
-                                DateTime Checker = CheckDt.AddDays(counter);
-                                var entrytest = from p in source.campsites
-                                                join p2 in Sites on p.campsite_id equals p2.CampsiteID
-                                                where p.AvailabilityDates.Contains(Checker) && p2?.CampsiteType?.ToUpper()?.Contains("GROUP") == false
-                                                orderby p.campsite_id
-                                                select new
-                                                {
-                                                    p.campsite_id,
-                                                    p2.CampsiteType,
-                                                    p2.CampsiteName,
-                                                    p2.Loop
-                                                };
-                                if (entrytest != null && entrytest.Count() > 0)
+                                string json = KampLibrary.function.generic.Data.NormalizeApiJsonData(r.ReadToEnd(), ParkID, CheckDt);
+                                source = JsonSerializer.Deserialize<AvailabilityEntries>(json);
+                                if (source?.campsites != null)
                                 {
-                                    foreach (var ThisEntry in entrytest)
+                                    foreach (var entry in source.campsites)
                                     {
-                                        //if (ThisEntry.campsite_id == "338")
-                                        //{
-                                        Console.WriteLine("***START***");
-                                        Console.WriteLine($"Date:   {Checker.ToShortDateString()} ({Checker.DayOfWeek})");
-                                        Console.WriteLine($"SiteID: {ThisEntry.campsite_id}");
-                                        Console.WriteLine($"Site:   {ThisEntry.CampsiteName}    Loop:{ThisEntry.Loop}");
-                                        Console.WriteLine($"Type:   {ThisEntry.CampsiteType}");
-                                        Console.WriteLine("***END***");
-                                        Console.WriteLine("");
-                                        //}
+                                        entry.GenerateDates(CheckDt);
+                                    }
+
+                                    Int32 DaysInCurrentMonth = DateTime.DaysInMonth(CheckDt.Year, CheckDt.Month);
+                                    int counter = 0;
+                                    while (counter < DaysInCurrentMonth)
+                                    {
+                                        DateTime Checker = CheckDt.AddDays(counter);
+                                        var entrytest = from p in source.campsites
+                                                        join p2 in Sites on p.campsite_id equals p2.CampsiteID
+                                                        where p.AvailabilityDates.Contains(Checker)
+                                                        orderby p.campsite_id
+                                                        select new
+                                                        {
+                                                            p.campsite_id,
+                                                            p2.CampsiteType,
+                                                            p2.CampsiteName,
+                                                            p2.Loop
+                                                        };
+                                        if (FilterOut != null && FilterOut.Count > 0)
+                                        {
+                                            foreach (var ThisFilterOut in FilterOut)
+                                            {
+                                                entrytest = entrytest.Where(p => p.CampsiteType?.ToUpper()?.Contains(ThisFilterOut) == false);
+                                            }
+                                        }
+
+                                        if (FilterIn != null && FilterIn.Count > 0)
+                                        {
+                                            foreach (var ThisFilterIn in FilterIn)
+                                            {
+                                                entrytest = entrytest.Where(p => p.CampsiteType?.ToUpper()?.Contains(ThisFilterIn) == true);
+                                            }
+                                        }
+
+                                        if (entrytest != null && entrytest.Count() > 0)
+                                        {
+                                            foreach (var ThisEntry in entrytest)
+                                            {
+                                                //if (ThisEntry.campsite_id == "338")
+                                                //{
+                                                Console.WriteLine($"Date:   {Checker.ToShortDateString()} ({Checker.DayOfWeek})");
+                                                Console.Write("URL:    ");
+                                                Console.ForegroundColor = ConsoleColor.Blue;
+                                                Console.Write($"https://www.recreation.gov/camping/campsites/{ThisEntry.campsite_id}");
+                                                Console.WriteLine();
+                                                Console.ResetColor();
+                                                Console.WriteLine($"Site:   {ThisEntry.CampsiteName}    Loop:{ThisEntry.Loop}");
+                                                Console.WriteLine($"Type:   {ThisEntry.CampsiteType}");
+                                                Console.WriteLine("");
+                                                //}
+                                            }
+                                        }
+                                        counter++;
                                     }
                                 }
-                                counter++;
                             }
                         }
                     }
+                    totalcounter++;
                 }
             }
         }
