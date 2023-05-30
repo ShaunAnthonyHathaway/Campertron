82c09a97b84ef123701c65d5124d7afd7a9ac534
Shaun Hathaway <Shaun.Hathaway@outlook.com>
2023-05-29 21:47:50 -0700

    removed unused

--- Campertron-CLI/Program.cs
+++ Campertron-CLI/Program.cs
@@ -1,12 +1,4 @@
-﻿using Microsoft.Extensions.DependencyModel;
-using Microsoft.Extensions.FileProviders;
-using System.Globalization;
-using System.IO;
-using System.Reflection;
-using System.Text;
-using System.Text.Json;
-
-class Program
+﻿class Program
 {
     static void Main(string[] args)
     {
