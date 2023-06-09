b08143ab6242d4a03f7d611d4475116c600274ce
Shaun Hathaway <Shaun.Hathaway@outlook.com>
2023-06-08 22:55:56 -0700

    removed usings

--- CampertronLibrary/class/ConsoleConfig.cs
+++ CampertronLibrary/class/ConsoleConfig.cs
@@ -1,11 +1,4 @@
-﻿using CampertronLibrary.function.generic;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-public class ConsoleConfig
+﻿public class ConsoleConfig
 {
     public class ConsoleConfigItem
     {
