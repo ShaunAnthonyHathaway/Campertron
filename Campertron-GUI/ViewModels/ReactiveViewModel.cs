c7d69f4aa1281a992b8a141776e030768d7c5a71
Shaun Hathaway <Shaun.Hathaway@outlook.com>
2023-08-10 21:42:01 -0700

    removed unused references

--- Campertron-GUI/ViewModels/ReactiveViewModel.cs
+++ Campertron-GUI/ViewModels/ReactiveViewModel.cs
@@ -1,12 +1,7 @@
 ﻿using CampertronLibrary.function.RecDotOrg.sqlite;
-using Microsoft.Extensions.DependencyModel;
 using ReactiveUI;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Campertron.ViewModels
 {
